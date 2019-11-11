a3f94ea8fa4a4301d018a496f4a96c83f1054185
Oscar Lang <oscar.lang98@gmail.com>
2019-11-11 10:48:14 +0000

    added ipvalidator to navbar

--- config/navbar/header.php
+++ config/navbar/header.php
@@ -6,7 +6,7 @@ return [
     // Use for styling the menu
     "wrapper" => null,
     "class" => "my-navbar rm-default rm-desktop",
- 
+
     // Here comes the menu items
     "items" => [
         [
@@ -37,6 +37,11 @@ return [
             "text" => "Om",
             "url" => "om",
             "title" => "Om denna webbplats.",
+        ],
+        [
+            "text" => "IP",
+            "url" => "ipvalidator1",
+            "title" => "Ip-validator.",
         ],
         [
             "text" => "Styleväljare",
