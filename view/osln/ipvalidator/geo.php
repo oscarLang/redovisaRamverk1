067a1f39614e1fa4138dfd5b8176a230e8aa7188
Oscar Lang <oscar.lang98@gmail.com>
2019-11-18 13:13:12 +0000

    added form action response

--- view/osln/ipvalidator/geo.php
+++ view/osln/ipvalidator/geo.php
@@ -14,7 +14,7 @@ if (isset($class)) {
 
 ?><article <?= classList($classes) ?>>
     <h1><?= $title ?></h1>
-    <form>
+    <form action="ipgeo/response">
         <input type="text" name="ip" value="<?= $ip ?>">
         <input type="submit" value="Send">
     </form>
