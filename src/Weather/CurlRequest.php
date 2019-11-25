3ec0a3052915841a5083100d0b8a0ffee2dabae5
Oscar Lang <oscar.lang98@gmail.com>
2019-11-25 14:05:46 +0000

    removed debug

--- src/Weather/CurlRequest.php
+++ src/Weather/CurlRequest.php
@@ -10,9 +10,6 @@ class CurlRequest
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
         $json = curl_exec($ch);
-        echo $json;
-        $error = curl_error($ch);
-        echo $error;
         curl_close($ch);
         $decoded = json_decode($json, true);
         return $decoded;
