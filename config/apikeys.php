34a1b2d838154cc27a220ff4f74f3f67bd6d0712
Oscar Lang <oscar.lang98@gmail.com>
2019-11-25 14:04:37 +0000

    added mapquest api-key

--- config/apikeys.php
+++ config/apikeys.php
@@ -4,5 +4,6 @@
  */
 return [
     "ipstack" => "bf34cd2fb019c1ba0300be5dbf7f3550",
-    "darksky" => "afe86cc35e3d9ceaecd06c12477658db"
+    "darksky" => "afe86cc35e3d9ceaecd06c12477658db",
+    "mapquest" => "2LnAvA3QPiUGBNS3EGOHSSydJUDMMsTp",
 ];
