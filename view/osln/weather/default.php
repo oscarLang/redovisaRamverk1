dbcea1de39b3b2610c2da09fdc7c1560add04407
Oscar Lang <oscar.lang98@gmail.com>
2019-11-25 12:56:24 +0000

    added form for checking weather

--- view/osln/weather/default.php
+++ view/osln/weather/default.php
@@ -14,5 +14,14 @@ if (isset($class)) {
 
 ?><article <?= classList($classes) ?>>
     <h1><?= $title ?></h1>
-    <iframe src="http://www.openstreetmap.org/export/embed.html" width="300" height="300"></iframe>
+    <form action="weather/response" method="get">
+        <input type="text" name="location" value=<?= $location ?>>
+        <input type="submit" value="submit">
+    </form>
+    <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=15.57518005371094%2C56.176222527214534%2C15.595264434814455%2C56.18418848712962&amp;layer=mapnik" style="border: 1px solid black">
+    </iframe>
+    <br/>
+    <small>
+        <a href="https://www.openstreetmap.org/#map=16/56.1802/15.5852">Visa större karta</a>
+    </small>
 </article>
