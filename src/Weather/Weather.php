e3e2d8845021ca0c309df33fe1d8fe25075ad80a
Oscar Lang <oscar.lang98@gmail.com>
2019-11-27 14:08:24 +0000

    added support to get last month weather

--- src/Weather/Weather.php
+++ src/Weather/Weather.php
@@ -1,23 +1,49 @@
 <?php
 
-namespace Osln\Geo;
+namespace Osln\Weather;
 
 use Anax\Commons\ContainerInjectableInterface;
 use Anax\Commons\ContainerInjectableTrait;
 
 class Weather
 {
-    public function initialize() : void
+    function __construct()
     {
         $this->curl = new CurlRequest();
         $this->config = new LoadConfig();
     }
     public function forecast($lat, $lon) : array
     {
-        $url = "https://api.darksky.net/forecast/%1\$s/%2\$s,%3\$s"
+        $url = "https://api.darksky.net/forecast/%1\$s/%2\$s,%3\$s?exclude=currently,flags,hourly,minutely&units=si";
         $key = $this->config->getKey("darksky");
         $urlFinal = sprintf($url, $key, $lat, $lon);
         $data = $this->curl->fetch($urlFinal);
         return [$data];
+    }
+
+    public function getLastMonth($lat, $lon) : array
+    {
+        $currentTimes = $this->getArrayOfDays();
+        $urls = [];
+        foreach ($currentTimes as $time) {
+            $key = $this->config->getKey("darksky");
+            $url = "https://api.darksky.net/forecast/%1\$s/%2\$s,%3\$s,%4\$s?exclude=currently,flags,hourly,minutely&units=si";
+            $urlFinal = sprintf($url, $key, $lat, $lon, $time);
+            $urls[] = $urlFinal;
+        }
+        // var_dump($urls);
+        $data = $this->curl->fetchMultiple($urls);
+        return [$data];
+    }
+
+    private function getArrayOfDays() : array
+    {
+        $times = [];
+        $now = time();
+        for ($i=0; $i < 30; $i++) {
+            $now -= (24 * 60 * 60);
+            $times[] = $now;
+        }
+        return $times;
     }
 }
