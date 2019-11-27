0a93eea80b9b1e8162265bf5e1eb115fe3a96a57
Oscar Lang <oscar.lang98@gmail.com>
2019-11-27 14:05:56 +0000

    added multicurl support

--- src/Weather/CurlRequest.php
+++ src/Weather/CurlRequest.php
@@ -17,12 +17,30 @@ class CurlRequest
 
     public function fetchMultiple($urls)
     {
-        $ch = curl_init($urlFinal);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $mh = curl_multi_init();
+        $chAll = [];
+        foreach ($urls as $url) {
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_multi_add_handle($mh, $ch);
+            $chAll[] = $ch;
+        }
 
-        $json = curl_exec($ch);
-        curl_close($ch);
-        $decoded = json_decode($json, true);
-        return $decoded;
+        $running = null;
+        do {
+            curl_multi_exec($mh, $running);
+        } while ($running);
+
+        foreach ($chAll as $ch) {
+            curl_multi_remove_handle($mh, $ch);
+        }
+        curl_multi_close($mh);
+
+        $response = [];
+        foreach ($chAll as $ch) {
+            $data = curl_multi_getcontent($ch);
+            $response[] = json_decode($data, true);
+        }
+        return $response;
     }
 }
