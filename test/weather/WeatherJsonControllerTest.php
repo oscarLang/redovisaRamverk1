4ab5c278f8d86a40c64dfe43fddbbbda2957c237
Oscar Lang <oscar.lang98@gmail.com>
2019-12-03 14:45:35 +0000

    created tests for weathercontroller, with a mock of weather

---
+++ test/weather/WeatherJsonControllerTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Osln\Weather;
+
+use Anax\DI\DIFactoryConfig;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test the SampleController.
+ */
+class WeatherJsonControllerTest extends TestCase
+{
+    protected function setUp()
+    {
+        global $di;
+
+        // Setup di
+        $this->di = new DIFactoryConfig();
+        $this->di->loadServices(ANAX_INSTALL_PATH . "/config/di");
+
+        // Use a different cache dir for unit test
+        $this->di->get("cache")->setPath(ANAX_INSTALL_PATH . "/test/cache");
+
+        $this->di->setShared("weather", "Osln\Weather\WeatherMock");
+
+        // View helpers uses the global $di so it needs its value
+        $di = $this->di;
+        $this->controller = new WeatherJsonController();
+        $this->controller->setDI($this->di);
+        $this->controller->initialize();
+    }
+    /**
+     * Test the route "index".
+     */
+    public function testIndexAction()
+    {
+
+        // Setup the controller
+
+        $req = $this->di->get("request");
+        $res = $this->controller->indexAction();
+
+        $this->assertInstanceOf("Anax\Response\Response", $res);
+        $this->assertInstanceOf("Anax\Response\ResponseUtility", $res);
+    }
+
+    public function testResponseAction()
+    {
+        // Setup the controller
+        // var_dump($this->di);
+        $req = $this->di->get("request");
+        $req->setGet("location", "158.174.29.41");
+
+        $res = $this->controller->responseActionGet();
+        $this->assertIsArray($res);
+
+        $req->setGet("location", "Kalmar");
+
+        $res = $this->controller->responseActionGet();
+        $this->assertIsArray($res);
+
+    }
+
+    public function testPreviousAction()
+    {
+        // Setup the controller
+        // var_dump($this->di);
+        $req = $this->di->get("request");
+        $req->setGet("location", "158.174.29.41");
+
+        $res = $this->controller->previousActionGet();
+        $this->assertIsArray($res);
+
+        $req->setGet("location", "Kalmar");
+
+        $res = $this->controller->previousActionGet();
+        $this->assertIsArray($res);
+    }
+}
